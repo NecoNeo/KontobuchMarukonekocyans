659c55cbae27946040e188042a92ef1ff39aa4a0
NecoNeo <neo2314@qq.com>
2021-06-19 10:40:40 +0800

    server init

---
+++ kontobuch-server/src/test/java/com/marukonekocyan/kontobuchserver/KontobuchServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marukonekocyan.kontobuchserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KontobuchServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
