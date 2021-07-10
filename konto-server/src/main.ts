ad18c5e97ee2dac58e3ef6f5bbdbe1b9ff91e010
NecoNeo <neo2314@qq.com>
2021-07-10 09:52:16 +0800

    server init

---
+++ konto-server/src/main.ts
@@ -0,0 +1,12 @@
+import express, { urlencoded } from 'express'
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  console.log('get req: ', { url: req.url, params: req.params, body: req.body, method: req.method })
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
