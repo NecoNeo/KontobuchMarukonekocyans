cab902bc78628d2dc8cfeea121519fb252e7b5a8
NecoNeo <neo2314@qq.com>
2021-07-15 21:48:22 +0800

    empty ios app

---
+++ kontobuchapp/kontobuchapp/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  kontobuchapp
+//
+//  Created by marukonekocyan on 2021/7/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView()
+        }
+    }
+}
---
+++ kontobuchapp/kontobuchapp/kontobuchappApp.swift
@@ -0,0 +1,17 @@
+//
+//  kontobuchappApp.swift
+//  kontobuchapp
+//
+//  Created by marukonekocyan on 2021/7/14.
+//
+
+import SwiftUI
+
+@main
+struct kontobuchappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
